351de71f124345735fb401e7cdac60ec472eb9ca
Ariana Gonzabay <arigalda@espol.edu.ec>
2024-11-25 13:55:20 -0500

    se agregó el componente IndicatorWeather

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid2'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import IndicatorWeather from './components/IndicatorWeather';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -13,10 +14,18 @@ function App() {
 
 
      {/* Indicadores */}
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+      <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
+      </Grid> 
+      <Grid size={{ xs: 12, xl: 3 }}>
+      <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+      <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+      <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+      </Grid>
 
       {/* Tabla */}
       <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
